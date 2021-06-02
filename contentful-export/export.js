f6151927a0b2810e33420db6a6d97db0c3799843
Simon Hanukaev <semenh@gmail.com>
2021-06-02 18:52:31 +0300

    decoupled footer from data

--- contentful-export/export.js
+++ contentful-export/export.js
@@ -6,22 +6,22 @@ const managementToken = process.env.CONTENTFUL_MANAGEMENT_TOKEN || process.argv[
 const spaceId = process.env.CONTENTFUL_SPACE_ID || process.argv[3];
 
 if (!managementToken || !spaceId) {
-    console.error('Contentful management token or space ID were not provided.\n\nUsage:\n./export.js <managementToken> <spaceId>\n');
-    process.exit(1);
+  console.error('Contentful management token or space ID were not provided.\n\nUsage:\n./export.js <managementToken> <spaceId>\n');
+  process.exit(1);
 }
 
 const options = {
-    spaceId: spaceId,
-    managementToken: managementToken,
-    exportDir: __dirname,
-    contentFile: 'export.json',
-    downloadAssets: true
+  spaceId: spaceId,
+  managementToken: managementToken,
+  exportDir: __dirname,
+  contentFile: 'export.json',
+  downloadAssets: true
 };
 
 contentfulExport(options)
-    .then((result) => {
-        console.log('Your space data:', result);
-    })
-    .catch((error) => {
-        console.log('Error exporting content:', error);
-    });
+  .then(() => {
+    console.log('Successfully export data');
+  })
+  .catch((error) => {
+    console.log('Error exporting content:', error);
+  });
--- src/components/sections/features.js
+++ src/components/sections/features.js
@@ -2,6 +2,31 @@ import Link from 'next/link';
 import Bolt from '../svgs/bolt';
 import Dots from '../svgs/dots';
 
+function FeatureItem(props) {
+  const feature = props.feature;
+  return (
+    <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+      <div>
+        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <Bolt size={12} />
+        </div>
+        <h6 className="mb-2 font-semibold leading-5">{feature.title}</h6>
+        <p className="mb-3 text-sm text-gray-900">{feature.subtitle}</p>
+      </div>
+      {feature.action && (
+        <Link href={feature.action.url}>
+          <a
+            aria-label={feature.action.alt}
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            {feature.action.label}
+          </a>
+        </Link>
+      )}
+    </div>
+  );
+}
+
 export default function Features(props) {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -26,25 +51,7 @@ export default function Features(props) {
       {props.features && props.features.length > 0 && (
         <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
           {props.features.map((feature, idx) => (
-            <div key={idx} className="flex flex-col justify-between p-5 border rounded shadow-sm">
-              <div>
-                <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-                  <Bolt size={12} />
-                </div>
-                <h6 className="mb-2 font-semibold leading-5">{feature.title}</h6>
-                <p className="mb-3 text-sm text-gray-900">{feature.subtitle}</p>
-              </div>
-              {feature.action && (
-                <Link href={feature.action.url}>
-                  <a
-                    aria-label={feature.action.alt}
-                    className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-                  >
-                    {feature.action.label}
-                  </a>
-                </Link>
-              )}
-            </div>
+            <FeatureItem key={idx} feature={feature} />
           ))}
         </div>
       )}
